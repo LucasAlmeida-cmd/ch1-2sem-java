6285437d1e4f417d0ffe335496a868a3b6a72103
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-05-26 16:21:16 -0300

    Commit Inicial

---
+++ src/test/java/com/example/GS1/Gs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
